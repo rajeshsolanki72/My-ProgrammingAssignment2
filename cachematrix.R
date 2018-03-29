000df4029e426a4165691e34e14280e667d190ec
rajeshsolanki72 <37664451+rajeshsolanki72@users.noreply.github.com>
2018-03-29 14:52:29 -0400

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,39 @@
+## the fuction makeChaeMatrix will create a special Matrix, which is a list conaining 
+## functions set the value of the Matrix, getthe value of matrix , set the value of invese,
+## get the invese matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+
+  inv_mat <- NULL
+  set <- function(y) {
+    x <<- y
+    inv_mat <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inv_mat <<- mean
+  getinverse <- function() inv_mat
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinvese = getinverse)
+}
+
+
+
+## Following function will calculate the invese of matric created in above function.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  function(x, ...) {
+    inv_mat <- x$getinvese()
+    if(!is.null(inv_mat)) {
+      message("getting cached data")
+      return(inv_mat)
+    }
+    data <- x$get()
+    inv_mat <- solve(data, ...)
+    x$setinverse(inv_mat)
+    inv_mat
+  
+}
+}
+
